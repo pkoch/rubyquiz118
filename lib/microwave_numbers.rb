5c6a22776f11f7cb2a8d55169a91bc220550cddc
Paulo Koch <paulo.koch@gmail.com>
2012-08-19 00:44:17 +0100

    Implement generate keypresses.

--- lib/microwave_numbers.rb
+++ lib/microwave_numbers.rb
@@ -1,4 +1,15 @@
+def generate_keypresses_for_seconds(seconds)
+  decomp_minutes, decomp_seconds = seconds.divmod(60)
+
+  return [seconds, decomp_minutes*100 + decomp_seconds].map do |e|
+    e.to_s + '*'
+  end.uniq
+end
+
 def microwave(seconds, opts={})
   skew = opts[:skew] || 0
-  return 0
+
+  options = generate_keypresses_for_seconds(seconds)
+
+  return options[0]
 end
--- test/test_microwave_numbers.rb
+++ test/test_microwave_numbers.rb
@@ -1,8 +1,22 @@
 require 'minitest/autorun'
 require 'microwave_numbers'
 
+class TestGenerateKeypressesForSeconds < MiniTest::Unit::TestCase
+  def test_0
+    assert_equal ['0*'], generate_keypresses_for_seconds(0)
+  end
+
+  def test_60
+    assert_equal ['60*', '100*'], generate_keypresses_for_seconds(60)
+  end
+
+  def test_71
+    assert_equal ['71*', '111*'], generate_keypresses_for_seconds(71)
+  end
+end
+
 class TestMicrowaveNumbers < MiniTest::Unit::TestCase
   def test_0
-    assert_equal 0, microwave(0)
+    assert_equal '0*', microwave(0)
   end
 end
