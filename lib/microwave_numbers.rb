c2f91ec60d6722f785cf70a4b8ade5170d7b7a86
Paulo Koch <paulo.koch@gmail.com>
2012-08-19 01:15:10 +0100

    Implement travel distances.

--- lib/microwave_numbers.rb
+++ lib/microwave_numbers.rb
@@ -6,10 +6,49 @@ def generate_keypresses_for_seconds(seconds)
   end.uniq
 end
 
+class UnknownKey < Exception; end
+
+def calculate_travel_distance(str)
+  positions = {
+    '1' => [0, 0],
+    '2' => [0, 1],
+    '3' => [0, 2],
+    '4' => [1, 0],
+    '5' => [1, 1],
+    '6' => [1, 2],
+    '7' => [2, 0],
+    '8' => [2, 1],
+    '9' => [2, 2],
+    '0' => [3, 1],
+    '*' => [3, 2],
+  }
+
+  previous_position = nil
+
+  str.chars.reduce(0) do |memo, c|
+    begin
+      current_position = positions.fetch(c)
+    rescue KeyError
+      raise UnknownKey.new(c)
+    end
+
+    result = 0
+    if !previous_position.nil?
+      result = memo + Math.sqrt(
+        (current_position[0] - previous_position[0])**2 +
+        (current_position[1] - previous_position[1])**2
+      )
+    end
+    previous_position = current_position
+
+    result
+  end
+end
+
 def microwave(seconds, opts={})
   skew = opts[:skew] || 0
 
-  options = generate_keypresses_for_seconds(seconds)
-
-  return options[0]
+  generate_keypresses_for_seconds(seconds).sort_by! do |e|
+    calculate_travel_distance(e)
+  end[0]
 end
--- test/test_microwave_numbers.rb
+++ test/test_microwave_numbers.rb
@@ -15,6 +15,28 @@ class TestGenerateKeypressesForSeconds < MiniTest::Unit::TestCase
   end
 end
 
+
+class TestCalculateTravelDistance < MiniTest::Unit::TestCase
+  def test_111
+    assert_equal 0, calculate_travel_distance('')
+  end
+
+  def test_111
+    assert_equal 0, calculate_travel_distance('111')
+  end
+
+  def test_123
+    assert_equal 2, calculate_travel_distance('123')
+  end
+
+  def test_bogus
+    assert_raises UnknownKey do
+      calculate_travel_distance('1a')
+    end
+  end
+end
+
+
 class TestMicrowaveNumbers < MiniTest::Unit::TestCase
   def test_0
     assert_equal '0*', microwave(0)
